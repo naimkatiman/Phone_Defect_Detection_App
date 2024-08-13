6b59d29fa1963d63f0c2102d531d13eaafe9f983
naimkatiman <naimkatiman@gmail.com>
2024-08-13 14:49:51 +0800

    chore: Update PhoneDefectApp to use a dummy model for testing

--- tests/test_phone_defect_app.py
+++ tests/test_phone_defect_app.py
@@ -17,7 +17,15 @@ class MockTk:
 class TestPhoneDefectApp(unittest.TestCase):
     def setUp(self):
         self.mock_tk = MockTk()
-        self.app = PhoneDefectApp(self.mock_tk)
+        # Create a dummy model file for testing
+        model = tf.keras.Sequential([tf.keras.layers.Dense(1, input_shape=(1,))])
+        model.save('dummy_model.h5')
+        self.app = PhoneDefectApp(self.mock_tk, model_path='dummy_model.h5')
+
+    def tearDown(self):
+        # Remove the dummy model file after tests
+        if os.path.exists('dummy_model.h5'):
+            os.remove('dummy_model.h5')
 
     def test_app_initialization(self):
         self.assertEqual(self.mock_tk.title_text, "Phone Defect Detection")
